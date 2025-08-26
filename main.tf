38bbdf8d0539b4f339641ba1e1d935497fa8f894
renukap866 <nataraj.renuka@gmail.com>
2025-08-26 11:19:31 +0000

    push

--- main.tf
+++ main.tf
@@ -20,5 +20,21 @@ resource "azurerm_resource_group" "rg" {
   tags     = var.resource_group_tags
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  address_space       = var.virtual_network_address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.virtual_network_tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_address_prefixes
+  service_endpoints   = var.subnet_service_endpoints
+}
+
 
 
--- variables.tf
+++ variables.tf
@@ -19,3 +19,38 @@ variable "resource_group_tags" {
   }
 }
 
+variable "virtual_network_name" {
+  description = "The name of the Virtual Network"
+  type        = string
+  default     = "tango-vnet"
+}
+variable "virtual_network_address_space" {
+  description = "The address space of the Virtual Network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+variable "subnet_name" {
+  description = "The name of the Subnet"
+  type        = string
+  default     = "tango-subnet"
+}
+variable "subnet_address_prefixes" {
+  description = "The address prefixes of the Subnet"
+  type        = list(string)
+  default     = ["10.0.0.0/24"]
+}
+variable "subnet_service_endpoints" {
+  description = "The service endpoints of the Subnet"
+  type        = list(string)
+  default     = ["Microsoft.Storage", "Microsoft.Sql"]
+}
+variable "virtual_network_tags" {
+  description = "Tags to assign to the Virtual Network"
+  type        = map(string)
+  default     = {
+    environment = "dev"
+    owner       = "terraform"
+  }
+}
+
+
